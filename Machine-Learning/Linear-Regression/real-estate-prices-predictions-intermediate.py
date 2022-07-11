67ccafb0f27b3d06dc8f5f2855949d54ca124819
James Kola Ojoawo <ojameskola03@gmail.com>
2022-07-11 13:40:58 +0100

    Add files via upload

---
+++ Machine-Learning/Linear-Regression/real-estate-prices-predictions-intermediate.py
@@ -0,0 +1,26 @@
+import numpy as np 
+
+# input values for three mökkis: 
+#  - size [m^2], 
+#  - size of the sauna [m^2], 
+#  - distance to water [m], 
+#  - number of indoor bathrooms, 
+#  - proximity of neighbors [m]
+X = [[66, 5, 15, 2, 500], 
+     [21, 3, 50, 1, 100], 
+     [120, 15, 5, 2, 1200]]
+
+# coefficient values
+c = [3000, 200 , -50, 5000, 100]
+
+def predict(X, c):
+    # write a loop that goes over the cabin data and for each cabin prints out 
+    # the predicted price of the cabin you can assume that the number of inputs
+    # and the number of coefficients are the same
+    
+    cabin_prices = np.multiply(X, c)
+    for eachcabin in cabin_prices:
+        price = np.sum(eachcabin)
+        print(price)
+
+predict(X, c)
