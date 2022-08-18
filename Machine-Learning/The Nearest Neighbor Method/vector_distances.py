ae2dfd89f79f7e3eda2fd28745707a5ddde69d02
James Kola Ojoawo <ojameskola03@gmail.com>
2022-08-18 21:39:56 +0100

    Update vector_distances.py

--- Machine-Learning/The Nearest Neighbor Method/vector_distances.py
+++ Machine-Learning/The Nearest Neighbor Method/vector_distances.py
@@ -1,7 +1,14 @@
+import math
 import numpy as np
 
-x_train = np.random.rand(10, 3)   # generate 10 random vectors of dimension 3
-x_test = np.random.rand(3)        # generate one more random vector of the same dimension
+x_train = np.array([[25, 2, 50, 1, 500], 
+                  [39, 3, 10, 1, 1000],    
+                  [82, 5, 20, 2, 120], 
+                  [130, 6, 10, 2, 600]])
+y_train = [127900, 222100,  268000, 460700]
+
+x_test = np.array([[115, 6, 10, 1, 560], [13, 2, 13, 1, 1000]])
+
 
 def dist(a, b):
     sum = 0
@@ -9,16 +16,11 @@ def dist(a, b):
         sum = sum + (ai - bi)**2
     return np.sqrt(sum)
 
-def nearest(x_train, x_test):
-    rand = []
-    
-    for eacharray in x_train:
-        d = dist(eacharray, x_test)
-        rand.append(d)
-    
-    min_distance  = min(rand)
-    nearest = rand.index(min_distance)
-    
-    return nearest
+n_train = len(x_train) # number of data points in the training set
 
-nearest(x_train, x_test)
+for test_item in x_test:
+    d = np.empty(n_train) # d will hold the distances between this test data point and all the training data points
+    for i, train_item in enumerate(x_train):
+        d[i] = dist(test_item, train_item)
+    nearest_index = np.argmin(d) # the nearest neighbour will be in y_train[nearest]
+    print(y_train[nearest_index])
